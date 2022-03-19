6f17697125c30df3b82adf411e9153c978ecccdf
Arpana <muriki.arpana@gmail.com>
2022-03-19 06:00:49 +0530

    First commit

---
+++ src/main/java/Rediff/VerifyHomePageTitle.java
@@ -0,0 +1,22 @@
+package Rediff;
+
+import org.testng.annotations.Test;
+
+public class VerifyHomePageTitle {
+
+	@Test(priority=1)
+	public void openBrowser()
+	{
+		System.out.println("Launch Application");
+	}
+	@Test(priority=2)
+	public void checkTitle()
+	{
+		System.out.println("Check Title");
+	}
+	@Test(priority=3)
+	public void closeBrowser()
+	{
+		System.out.println("Close Application.");
+	}
+}
