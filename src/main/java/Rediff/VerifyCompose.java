6c3f671c8a64ad2b5ef5aa9afd08e553dffc607e
Arpana <muriki.arpana@gmail.com>
2022-03-23 05:45:07 +0530

    Third Commit

---
+++ src/main/java/Rediff/VerifyCompose.java
@@ -0,0 +1,10 @@
+package Rediff;
+
+public class VerifyCompose {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Verify compose email...");
+	}
+
+}
